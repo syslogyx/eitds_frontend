67870ef9fe071704043fb146cde2f4ec902cde3b
Monica <monica.j@syslogyx.com>
2018-09-14 11:42:13 +0530

    solved validation issue on generate sticker page

--- controller/generateStickerCtrl.js
+++ controller/generateStickerCtrl.js
@@ -8,25 +8,31 @@ app.controller('generateStickerCtrl', function ($scope,menuService,services,$coo
 
 		ges.generateSticker = function () {
 			// return console.log({user_id:loggedInUser.id,productId:ges.productId});
-			var promise = services.generateSticker({product_id:ges.productId});
-			promise.success(function (result) {
-				debugger;
-				if(result.status_code == 200){
-					Utility.stopAnimation();
-						ges.finalId = result.data[0].finalId;
-						ges.seriesName = result.data[0].seriesName;
-						
-				}else{
-					Utility.stopAnimation();
-						ges.finalId = '';
-						toastr.error(result.message, 'Sorry!');
-				}
-			});
+			if($("#deviceForm").valid()){
+				var promise = services.generateSticker({product_id:ges.productId});
+				promise.success(function (result) {
+					debugger;
+					if(result.status_code == 200){
+						Utility.stopAnimation();
+							ges.finalId = result.data[0].finalId;
+							ges.seriesName = result.data[0].seriesName;
+							
+					}else{
+						Utility.stopAnimation();
+							ges.finalId = '';
+							ges.seriesName = "";
+							toastr.error(result.message, 'Sorry!');
+					}
+				});
+			}
+			
 		}
 
 		ges.clearForm=function(){
 			ges.productId=null;
 			ges.generatedId=null;
+			ges.finalId = '';
+			ges.seriesName = "";
 		}
 
 });
